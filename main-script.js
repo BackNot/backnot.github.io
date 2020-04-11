1afd28af54ca09c371ae3f46ddf45f00cdade4a7
backnot <alexashkov1@gmail.com>
2020-04-12 02:02:06 +0300

    Design changes

--- main-script.js
+++ main-script.js
@@ -1,82 +1,28 @@
 $(document).ready(function() {
 loadPage("#page-content", "home", mainPageTextAnimation);
-var timeout1;
-var timeout2;
-
-    // var showTextLetterByLetter = function (target, message, index, interval) {   
-    //     if (index < message.length) {
-    //         $(target).append("_");
-    //          timeout1 = setTimeout(function() {
-    //             $(target).text($(target).text().substring(0,($(target).text().length - 1)));
-    //             $(target).append(message[index++]);
-    //             timeout2 = setTimeout(function () { 
-    //                 showTextLetterByLetter(target, message, index, interval); }, interval);
-    //         }, 25)
-    //     }
-    //     else
-    //         makeUnderscoreFlashing(target);
-    // }
-
-    // function makeUnderscoreFlashing(target) {
-    //     target = $(target);
-    //     setInterval(function(){
-    //         $(target).append("_");
-    //         setTimeout(function () { 
-    //             $(target).text($(target).text().substring(0,($(target).text().length - 1)));
-
-    //     }, 200)},300);
-    // }
-    
-    // showTextLetterByLetter("#index-text", $("#index-dummy-text").text(), 0, 1);
-
-    var currentdate = new Date(); 
-    var datetime = {};
-    datetime.date =  currentdate.getDate() + "/"
-                    + "0" + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear();
-
-    var minutes = checkTime(currentdate.getMinutes());
-    var hours = checkTime(currentdate.getHours());
-
-    datetime.time = hours + ":" + minutes;
-    $("#time").text(datetime.time);
-
-    setInterval(function() {
-        var currentdate = new Date(); 
-        var minutes = checkTime(currentdate.getMinutes());
-        var hours = checkTime(currentdate.getHours());
-    
-        datetime.time = hours + ":" + minutes;
-        $("#time").text(datetime.time);
-    }, 60000);
-   
-    function checkTime(i) {
-        if (i < 10) {
-          i = "0" + i;
-        }
-        return i;
-      }
 
     function mainPageTextAnimation() {
         console.log('here');
-    $("#avatar" ).animate({
-        opacity: 1.00,
-        left: "50%",
-    }, 2000);
+        $("#avatar" ).animate({
+            opacity: 1.00,
+            left: "50%",
+        }, 2000);
 
-    $( "#index-text" ).animate({
-        opacity: 1.00,
-    }, 2500);
+        $( "#index-text" ).animate({
+            opacity: 1.00,
+        }, 2500);
+
+        setTimeout(function () {
+            $(".background-image").animate({
+                opacity: 1.00,
+            }, 1500);
+        }, 1500);
+
     }
 
     // Events
     function loadPage(destination, page, callback)
-    {
-        if (timeout1)
-            clearTimeout(timeout1);
-        if (timeout2)
-            clearTimeout(timeout2);
-            
+    {       
         $.ajax({
             url: page + ".html",
             method: "GET",
@@ -116,7 +62,7 @@ var timeout2;
 
     function enableNightMode(addSessionStorage)
     {
-        $(".container-fluid").addClass("black-background");
+        $(".container-fluid").removeClass("gradient-background").addClass("black-background");
         $(".container-fluid").addClass("text-white");
         $(".header").removeClass("lightgrey-bg");
         $(".black-border").addClass("white-border").removeClass("black-border");
@@ -127,7 +73,7 @@ var timeout2;
 
     function disableNightMode(removeSessionStorage)
     {
-        $(".container-fluid").removeClass("black-background");
+        $(".container-fluid").removeClass("black-background").addClass("gradient-background");
         $(".container-fluid").removeClass("text-white");
         $(".header").addClass("lightgrey-bg");
         $(".white-border").addClass("black-border").removeClass("white-border");
@@ -146,11 +92,5 @@ var timeout2;
     if (localStorage.getItem("nightmode") != null)
         enableNightMode(false);
 
-// Clock
-    var opt = new AnalogClockOption(); 
-    opt.width = 100;
-    var clock = new AnalogClock("clock", opt);
-   $("#clock").draggable();
-   
 
 });
